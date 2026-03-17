de11857ec8ebfd5ab79186f36522b6103428fad4
DeepakSekar003 <deepaksekar003@gmail.com>
2026-03-17 17:44:48 +0530

    made a changes in api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,12 +1,29 @@
 const API_URL = "http://35.173.15.130:8080/api/expenses";
 
 export async function getExpenses() {
-  const res = await fetch(API_URL);
+  const res = await fetch(API_URL, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch expenses");
+  }
+
   return res.json();
 }
 
 export async function deleteExpense(id) {
-  await fetch(`${API_URL}/${id}`, {
-    method: "DELETE"
+  const res = await fetch(`${API_URL}/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json"
+    }
   });
+
+  if (!res.ok) {
+    throw new Error("Failed to delete expense");
+  }
 }
