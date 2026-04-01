453fef1bca4fb4256c5e00b83af1acce47222aed
DeepakSekar003 <deepaksekar003@gmail.com>
2026-04-01 15:38:35 +0530

    made a changes in deploy files

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-cd /home/ec2-user/app
+cd /home/ubuntu/app
 
-echo "Current files:"
-ls -l
-
-# kill old app
 pkill -f expense-tracker || true
 
-# find jar dynamically (BEST PRACTICE)
-JAR_FILE=$(ls *.jar)
-
-echo "Running $JAR_FILE"
-
-nohup java -jar $JAR_FILE > app.log 2>&1 &
+nohup java -jar *.jar > app.log 2>&1 &
