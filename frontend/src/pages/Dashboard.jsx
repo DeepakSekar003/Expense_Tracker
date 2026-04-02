ca863358c3bde57b518b29d374d251a7e48751a1
DeepakSekar003 <deepaksekar003@gmail.com>
2026-04-02 18:29:11 +0530

    made a chagnes in dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -50,7 +50,7 @@ export default function Dashboard() {
       <div className="flex-1 p-8">
 
         <h1 className="text-2xl font-bold mb-6">
-          Dashboard
+          Dash Board
         </h1>
 
         {/* Cards */}
