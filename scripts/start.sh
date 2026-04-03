7dc8815c92ea0649b7e5f586a1ee8e2359acdf81
DeepakSekar003 <deepaksekar003@gmail.com>
2026-04-03 11:55:48 +0530

    made a changes in deploy files

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-cd /home/ubuntu/app
-
+echo "Stopping old backend..."
 pkill -f expense-tracker || true
 
+echo "Starting backend..."
+cd /home/ubuntu/backend
 nohup java -jar *.jar > app.log 2>&1 &
+
+echo "Deploying frontend..."
+sudo rm -rf /var/www/html/*
+sudo cp -r /home/ubuntu/app/frontend/build/* /var/www/html/
+
+sudo systemctl restart nginx
