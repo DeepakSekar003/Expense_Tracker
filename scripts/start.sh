104b8f583a41bff01183d702235dc18676452b9d
DeepakSekar003 <deepaksekar003@gmail.com>
2026-04-03 12:12:20 +0530

    made a changes in sh file

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,11 +4,15 @@ echo "Stopping old backend..."
 pkill -f expense-tracker || true
 
 echo "Starting backend..."
-cd /home/ubuntu/backend
-nohup java -jar *.jar > app.log 2>&1 &
+cd /home/ubuntu/app/backend/target
+
+nohup java -jar *.jar > /home/ubuntu/app/backend/app.log 2>&1 &
 
 echo "Deploying frontend..."
 sudo rm -rf /var/www/html/*
 sudo cp -r /home/ubuntu/app/frontend/build/* /var/www/html/
 
+echo "Restarting nginx..."
 sudo systemctl restart nginx
+
+echo "Deployment completed!"
